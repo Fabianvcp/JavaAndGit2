ece3ed1afad815bfdaebca7581882fbd0778aa09
Fabian <gallardofabianvcpz@gmail.com>
2021-06-08 10:54:36 -0300

    cambios hechos en los cambios secundarios

--- src/Principal.java
+++ src/Principal.java
@@ -2,13 +2,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Proyecto en git" );
         /*Ahora vamos hacer lo siguiente
-        * 1- agregar los cambios git add .
-        * 2- darle un nombre a los cambios git commit
-        * 3- agragar el repósitorio remoto
-        * 4- subir los cambios
-        * 5- bajarlos
-        * 6- crear otra rama
-        * 7- subir cambios a esa rama
+        * 1- subir los cambios
+        * 2- antes subir lo cambio simpre verifica la rama que estas
         * */
     }
 }
